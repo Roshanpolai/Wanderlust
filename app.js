6c487df89a4680306036587b6bc86371511698fd
Roshan polai <roshanpolai3@gmail.com>
2026-01-23 22:46:59 +0530

    Implement user authentication with Passport.js and add flash messaging for notifications

--- app.js
+++ app.js
@@ -5,19 +5,22 @@ const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-
 const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+const flash = require("express-flash");
+
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 // Routes
 const listings = require("./routes/listing.js");
-const reviews  = require("./routes/review.js");
+const reviews = require("./routes/review.js");
 
 // MongoDB URL
 const MONGO_URL = "mongodb://127.0.0.1:27017/test";
 
-
-
-// MongoDB Connection
+// DB Connection
 main()
   .then(() => console.log("connected to DB"))
   .catch((err) => console.log(err));
@@ -26,42 +29,76 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
-
-// View Engine Setup
+// View Engine
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-
-// Middlewares
+// Basic Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 
+// Session
+const sessionOptions = {
+  secret: "MySecretisConsistenct",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
 
-// Basic Route
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(session(sessionOptions));
+app.use(flash());
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Flash locals
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
 });
 
+// Home route 
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
 
-// App Routes
+// Demo user route
+app.get("/demouser", async (req, res) => {
+  let fakeUser = new User({
+    username: "chris",
+    email: "chris@example.com",
+  });
+
+  let registeredUser = await User.register(fakeUser, "chrispassword");
+  res.send(registeredUser);
+});
+
+// App routes
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
 
-
-// 404 Handler
+// 404
 app.use((req, res, next) => {
   next(new ExpressError(404, "Page Not Found"));
 });
 
-
-// Global Error Handler
+// Error handler
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong" } = err;
   res.status(statusCode).render("error.ejs", { message });
 });
-
 
 // Server
 app.listen(8000, () => {
