25fbc3da0bfcccb3d3aeca81253772893e791252
Roshan polai <roshanpolai3@gmail.com>
2026-02-02 23:41:29 +0530

    Refactor user and listing routes to use route chaining

--- routes/listing.js
+++ routes/listing.js
@@ -5,48 +5,40 @@ const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
 
-
-
-//Index route
-router.get("/", 
-  wrapAsync(listingController.index));
+router
+  .route("/")
+  .get(wrapAsync(listingController.index))
+  .post(
+    isLoggedIn,
+    validateListing,
+    wrapAsync(listingController.createListing),
+  );
 
 //New listing form
-router.get("/new",
-  isLoggedIn,
-  listingController.renderNewForm);
+router.get("/new", isLoggedIn, listingController.renderNewForm);
 
-// Show Route
-router.get("/:id",
-  wrapAsync(listingController.showListing));
+router
+  .route("/:id")
+  .get(wrapAsync(listingController.showListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    validateListing,
+    wrapAsync(listingController.updateListing)
+  )
+  .delete(
+    isLoggedIn,
+    isOwner,
+    wrapAsync(listingController.deleteListing)
+  );
 
-//Create route
-router.post("/",
-  isLoggedIn,
-  validateListing,
-  wrapAsync(listingController.createListing),
-);
 
 //Edit Route
-router.get("/:id/edit",
+router.get(
+  "/:id/edit",
   isLoggedIn,
   isOwner,
   wrapAsync(listingController.renderEditForm),
-);
-
-//Update Route
-router.put("/:id",
-  isLoggedIn,
-  isOwner,
-  validateListing,
-  wrapAsync(listingController.updateListing),
-);
-
-//Delete Route
-router.delete("/:id",
-  isLoggedIn,
-  isOwner,
-  wrapAsync(listingController.deleteListing),
 );
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -6,26 +6,23 @@ const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 const userController = require("../controllers/users.js");
 
-router.get("/signup", userController.renderSignupForm);
+router
+  .route("/signup")
+  .get(userController.renderSignupForm)
+  .post(wrapAsync(userController.signup));
 
-router.post(
-  "/signup",
-  wrapAsync(userController.signup),
-);
-
-router.get("/login", userController.renderLoginForm);
-
-router.post(
-  "/login",  
-  saveRedirectUrl,
-  passport.authenticate("local", {
-    failureFlash: true,
-    failureRedirect: "/users/login",
-  }),
-  userController.login
-);  
+router
+  .route("/login")
+  .get(userController.renderLoginForm)
+  .post(
+    saveRedirectUrl,
+    passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/users/login",
+    }),
+    userController.login,
+  );
 
 router.get("/logout", userController.logout);
-
 
 module.exports = router;
